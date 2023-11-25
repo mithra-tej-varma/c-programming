9e0a9abeacfc53b1e15d4bb778d68f1681eaf1ed
mithra-tej-varma <tejvarmamithra@gmail.com>
2023-11-25 06:11:56 +0530

    second program practise in c addition of two numbers

---
+++ 01_practise/02_practise.c
@@ -0,0 +1,10 @@
+// c program to print addition of two numbers
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter a and b values");
+    scanf("%d%d",&a,&b);
+    c = a+b;
+    printf("%d + %d = %d",a,b,c);
+    return 0;
+}
